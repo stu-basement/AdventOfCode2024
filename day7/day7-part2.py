73880e137c2d9793788083bee5abbe971261277c
Stu Fleming <stu@thebasement.nz>
2024-12-08 11:07:53 +1300

    Day 7 solutions part 1 and part 2

---
+++ day7/day7-part2.py
@@ -0,0 +1,63 @@
+import itertools
+
+results = []
+values = []
+operations = []
+success = []
+
+def operatorCombo(op, n):
+   yield from itertools.product(*([op] * n))
+
+def calc(op, term1, term2):
+    if (op[0] == "+"):
+      return term1 + term2
+    elif (op[0] == "*"):
+        return term1 * term2
+    elif (op[0] == "|"):
+        return (term1 * (10 ** len(str(term2)))) + term2
+    else:
+        return None
+
+f = open('input')
+count = 0
+maxTerms = 0
+for line in f.readlines():
+    x = line.partition(":")
+    results.append(int(x[0]))
+    terms = list(map(int, x[2].split()))
+    values.append(terms)
+    maxTerms = max(maxTerms, len(terms))
+
+print(f"Longest is {maxTerms} need {maxTerms - 1} operations combos")
+
+for opLength in range(0, maxTerms - 1):
+    for operators in operatorCombo("+*|",opLength+1):
+        operations.append(list(''.join(operators)))
+
+print("Operations")
+for op in operations:
+    print(op)
+
+def calcEquation(values, combo, expectedResult):
+    result= values[0]
+    for v in range(1, len(values)):
+        result = calc(combo[v-1], result, values[v])
+    return (result == expectedResult)
+
+value = 0
+for r in range(0, len(results)):
+    validResult = False
+    for combo in operations:
+        if (len(combo) == len(values[r]) - 1):
+            validResult = calcEquation(values[r], combo, results[r])
+            if (validResult):
+                print(f"SUCCESS {results[r]}: {values[r]} {combo}")
+                success.append(results[r])
+                break
+
+total = 0
+print(f"SUCCESS: {success}")
+print(f"Valid results {len(success)}")
+for s in success:
+    total += s
+print(f"PART1 total calibration result {total}")
