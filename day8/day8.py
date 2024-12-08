832eb7428a74797dd520c78311d996952df74816
Stu Fleming <stu@thebasement.nz>
2024-12-09 07:39:29 +1300

    Part 2 solution

--- day8/day8.py
+++ day8/day8.py
@@ -52,3 +52,49 @@ for row in areaMap:
     print(row)
 
 print(f"Locations {locationCount}")
+
+print("PART2")
+areaMap = originalMap.copy()
+antennaLocations = set()
+for y in range(0, len(areaMap)):
+    for x in range(0, len(areaMap[0])-1):
+        if areaMap[y][x] != '.':
+            antennaLocations.add((x,y,areaMap[y][x]))
+print(f"Antenna locations: {antennaLocations}")
+
+edges = set()
+for id, antenna in enumerate(antennaLocations):
+    print(f"Antenna {antenna[2]} at {antenna[0]},{antenna[1]}") 
+    for id1, antenna1 in enumerate(antennaLocations):
+        if (antenna1[2] == antenna[2]):
+            dx = antenna1[0] - antenna[0]
+            dy = antenna1[1] - antenna[1]
+            if not (dx == 0 and dy == 0):
+                edges.add((antenna[0], antenna[1], antenna[2], dx, dy))
+
+print(f"Edges: {edges}")
+for e in edges:
+    x = e[0]
+    y = e[1]
+    dx = e[3]
+    dy = e[4]
+
+    print(f"Process edge {e}")
+    while onMap(x, y):
+        print(f"Antinode at {x},{y}")
+        antinodeLocations.add((x,y))
+        x += dx
+        y += dy
+
+
+locationCount = 0
+for l in antinodeLocations:
+    ly = l[1]
+    lx = l[0]
+    areaMap[ly] = areaMap[ly][:lx] + "#" + areaMap[ly][lx+1:]
+    locationCount += 1
+
+for row in areaMap:
+    print(row)
+
+print(f"Locations {locationCount}")
