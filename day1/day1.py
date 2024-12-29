41eb0cf6810f6dc55ff77898b733d4dc5b9910b6
Stu Fleming <stu@thebasement.nz>
2024-12-29 15:02:49 +1300

    Refactor for pylint changes, including using generators

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,11 @@
+""" AdventOfCode Day 1 """
+
 from collections import Counter
 
 listA = []
 listB = []
 
-with open("input", "r") as f:
+with open("input", "r", encoding="utf-8") as f:
     for line in f.readlines():
         a, b = map(int, line.replace('\n', '').split())
         listA.append(a)
@@ -12,7 +14,7 @@ with open("input", "r") as f:
 # Part 1
 listA.sort()
 listB.sort()
-totalDistance = sum([abs(x[1] - x[0]) for x in zip(listA, listB)])
+totalDistance = sum(list(abs(x[1] - x[0]) for x in zip(listA, listB)))
 
 print(f"Total distance {totalDistance}")
 
@@ -20,6 +22,6 @@ print(f"Total distance {totalDistance}")
 # Use a counter to avoid exploding time complexity to O(n^2) using B.count(a)
 # https://www.reddit.com/r/adventofcode/comments/1h3vp6n/comment/lzw63fl/
 cnt = Counter(listB)
-similarity = sum([x * cnt[x] for x in listA])
+similarity = sum(list(x * cnt[x] for x in listA))
 
 print(f"Similarity score {similarity}")
