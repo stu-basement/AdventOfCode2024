aa13209bb97e3f5b4fda14207b4df41cc4c6281f
Stu Fleming <stu@thebasement.nz>
2024-12-30 12:09:06 +1300

    Refactored to add regexp for part 2 and tighter regexp for digit matching (1-3 digits allowed)

--- day3/day3.py
+++ day3/day3.py
@@ -2,9 +2,17 @@
 from re import findall
 
 totalPart1 = 0
+totalPart2 = 0
+enabled = True
 with open("input", "r", encoding="utf-8") as f:
-   for line in f.readlines():
-       for a, b in findall(r"mul\((\d+),(\d+)\)", line):
-           totalPart1 += int(a) * int(b)
+    for line in f.readlines():
+        for a, b, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", line):
+            if do or dont:
+                enabled = bool(do)
+            else:
+                totalPart1 += int(a) * int(b)
+                if enabled:
+                    totalPart2 += int(a) * int(b)
 
 print(f"PART1 total: {totalPart1}")
+print(f"PART2 total: {totalPart2}")
