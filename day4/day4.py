592e9a77d7c7fed977d432e49349e991928558c4
Stu Fleming <stu@thebasement.nz>
2024-12-30 13:42:37 +1300

    Refactored solution for part 1 using sets for locations of X

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,10 @@
+""" AdventOfCode Day 4 """
+from collections import defaultdict
 
 xmas = ['X', 'M', 'A', 'S']
 mas = ['M', 'A', 'S']
 wordSquare = []
+directions = [ [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1] ]
 
 def findWord(startX, startY, xDelta, yDelta, word):
     foundCount = 0
@@ -14,37 +17,35 @@ def findWord(startX, startY, xDelta, yDelta, word):
         startY += yDelta
     return (foundCount == len(word))
 
-with open("input", "r") as f:
-    for line in f.readlines():
-        print(line)
-        wordSquare.append(line)
-
 foundCount = 0
+x_spots= set()
+letter_spots = set()
+y = 0
+with open("input", "r", encoding="utf-8") as f:
+    line = list(f.readline().replace('\n', ''))
+    while len(line) > 0:
+        for x in enumerate(line):
+            if x[1] == 'X':
+                x_spots.add( ( x[0], y ) )
+            elif x[1] != '.':
+                letter_spots.add( (x[1], x[0], y) )
 
-print(f"XMAS {len(xmas)}")
-print(f"Wordsquare rows {len(wordSquare)}")
-print(f"Wordsquare cols {len(wordSquare[0])}")
+        line = list(f.readline().replace('\n', ''))
+        y += 1
 
-for startY in range(0, len(wordSquare)):
-   for startX in range(0, len(wordSquare[0])):
-      if (findWord(startX, startY, 0, -1, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, 1, -1, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, 1, 0, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, 1, 1, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, 0, 1, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, -1, 1, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, -1, 0, xmas)):
-         foundCount += 1
-      if (findWord(startX, startY, -1, -1, xmas)):
-         foundCount += 1
+for spot in x_spots:
+    for d in directions:
+        for letter in enumerate(mas):
+            if (letter[1], \
+                    spot[0] + (d[0] * (letter[0] + 1)), \
+                    spot[1] + (d[1] * (letter[0] + 1)) ) not in letter_spots:
+                break
 
-print(f"Total found (part 1): {foundCount}") 
+            if letter[0] == len(mas) - 1:
+                foundCount += 1
+
+print(f"PART1: total found {foundCount}")
+exit()
 
 print("Part 2")
 foundCount = 0
