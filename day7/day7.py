2d12c42f440bc577e295262b2cbb5834fa96fad3
Stu Fleming <stu@thebasement.nz>
2025-01-01 19:13:07 +1300

    Refactored for more effciient solution by working backwards from result

--- day7/day7.py
+++ day7/day7.py
@@ -1,61 +1,69 @@
 """ AdventOfCode Day 7 """
-import itertools
 import math
 
-results = []
-values = []
-operations = []
-success = []
+def findSolution(eqs, operators):
+    """ find a solution to an equation by using *, +, | operators """
+    # Work backwards to prune the number of possibilities
+    # Process terms in reverse order
+    # At each step, record the partial result and stop when this becomes zero (solution found)
+    # concatenation is only possible if the last digits of the partial result
+    # are the same as the last digits of the next term
+    # multiplication is only possible if the partial result is exactly divisible by the next term
+    # addition is only possible if subtracting the next term leads to a positive result
+    # Node: (partial result, [remaining terms] )
+    calibration_result = 0
+    for partial, terms in eqs:
+        result = partial
+        stack = []
 
-def operatorCombo(op, n):
-    """ Return the combinations of all operands for the number of terms """
-    yield from itertools.product(*([op] * n))
+        current = (result, terms)
+        stack.append(current)
 
-def calc(op, term1, term2):
-    """ Perform the calculation term1 op term2 """
-    if op == "+":
-        return term1 + term2
+        while stack:
+            partial, terms = stack.pop()
 
-    if op == "*":
-        return term1 * term2
+            # if our partial result becomes zero and we have used all the terms,
+            # this sequence is a correct solution
+            if partial == 0 and not terms:
+                calibration_result += result
+                break
 
-    if op == '|':
-        return (term1 * (10 ** (math.floor(math.log10(term2)) + 1))) + term2
+            if not terms:
+                continue
 
-    return None
+            next_term = terms[0]
+            for combo in operators:
+                # can't have concatenation if last digits of result not same as term
+                if combo == '|':
+                    if (partial - next_term) % \
+                            (10 ** (math.floor(math.log10(next_term)) + 1)) != 0:
+                        continue
+                    next_node =  ( (partial - next_term) // \
+                        (10 ** (math.floor(math.log10(next_term)) + 1)), terms[1:])
+                    stack.append(next_node)
+
+                # can't have multiplication if result not exactly divisible by term
+                if combo == '*':
+                    if partial % next_term != 0:
+                        continue
+                    next_node = (partial // next_term, terms[1:])
+                    stack.append(next_node)
+
+                # can't have addition if subtraction would result in a negative number
+                if combo == '+':
+                    if partial - next_term < 0:
+                        continue
+                    next_node = (partial - next_term, terms[1:])
+                    stack.append(next_node)
+    return calibration_result
 
 equations = []
 with open('input', 'r', encoding='utf-8') as f:
     for line in f.readlines():
         x = line.replace('\n', '').split(':')
-        equations.append( (int(x[0]), list(map(int, x[1].split()))) )
+        t = list(map(int, x[1].split()))
+        t.reverse()
+        equations.append( (int(x[0]), t) )
 
-calibration_result = 0
-for e in equations:
-    for combo in operatorCombo('+*', len(e[1])):
-        result = e[1][0]
-        for t in enumerate(e[1][1:]):
-            result = calc(combo[t[0]-1], result, t[1])
-
-        if result == e[0]:
-            calibration_result += e[0]
-            break
-print(f"PART1 total calibration result {calibration_result}")
-
-calibration_result = 0
-for e in equations:
-    for combo in operatorCombo('+*|', len(e[1])):
-        result = e[1][0]
-
-        for t in enumerate(e[1][1:]):
-            result = calc(combo[t[0]-1], result, t[1])
-
-            # Early exit if we exceed the expected result before using all the terms
-            if result > e[0]:
-                break
-
-        if result == e[0]:
-            calibration_result += e[0]
-            break
-
-print(f"PART2 total calibration result {calibration_result}")
+print(f"PART1 total calibration result {findSolution(equations, '*+')}")
+print(f"PART2 total calibration result {findSolution(equations, '*+|')}")
