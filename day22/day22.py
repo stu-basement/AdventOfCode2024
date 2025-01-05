dd0fc4aa9ce86ee6ac1681fdbde815beb80c490f
Stu Fleming <stu@thebasement.nz>
2025-01-06 11:58:32 +1300

    Refactored to use sliding window

--- day22/day22.py
+++ day22/day22.py
@@ -1,60 +1,64 @@
+""" AdventOfCode Day 22 """
+from more_itertools import windowed
+
 buyerSeeds = []
 bananas = {}
 
-def mix(s, n):
-    return n ^ s
-
-def prune(n):
-    return n % 16777216
-
 def nextSecret(s):
-    ns = ((s * 64) ^ s) % 16777216
-    ns = ((ns // 32) ^ ns) % 16777216
-    ns = ((ns * 2048) ^ ns) % 16777216
+    """ Use a PRNG to generate the secret prices """
+    next_s = ((s * 64) ^ s) % 16777216
+    next_s = ((next_s // 32) ^ next_s) % 16777216
+    next_s = ((next_s * 2048) ^ next_s) % 16777216
 
     return ns
 
-with open("input", "r") as f:
-    line = f.readline()
-    while (len(line) > 1):
-        buyerSeeds.append(int(line.replace('\n', '')))
-        line = f.readline()
+with open("input", "r", encoding="utf-8") as f:
+    line = f.readline().replace('\n', '')
+    while line:
+        buyerSeeds.append(int(line))
+        line = f.readline().replace('\n','')
+
+# Simply generate 2000 secret numbers and take the total
+total_secrets = 0
+for b in buyerSeeds:
+    ns = b
+    for n in range(2000):
+        ns = nextSecret(ns)
+    total_secrets += ns
+print(f"PART1: Total of 2000th secrets: {total_secrets}")
 
 # PART 2
 
 # For each buyer, get all the sequences
-# and the total bananas they would yield 
+# and the total bananas they would yield
 # dictionary of key: sequence, value: total bananas
 bananas = {}
 
 for b in buyerSeeds:
     buyerSequences = {}
 
-    # generate the secret numbers
+    # generate the first digits of prices
     ns = b
-    prices = [ ns ]
+    prices = [ ns % 10 ]
     for i in range(2000):
         ns = nextSecret(ns)
-        prices.append(ns)
-
-    # generate the first digits (prices)
-    firstDigits = [x % 10 for x in prices]
+        prices.append( ns % 10 )
 
     # calculate the price changes
-    deltas = [firstDigits[i+1] - firstDigits[i] for i in range(len(firstDigits) - 1)]
+    deltas = [str(prices[i+1] - prices[i]) for i in range(len(prices) - 1)]
 
-    for i in range(len(deltas) - 3):
-        sequence = str(deltas[i])+str(deltas[i+1])+str(deltas[i+2])+str(deltas[i+3])
-
+    # use a sliding window to find the pattern
+    for sn, sequence in enumerate(windowed(deltas, n=4, step=1)):
         # dictionary should contain the total bananas for this buyer
         # for this sequence
         if sequence not in buyerSequences:
-            buyerSequences[ sequence ] = firstDigits[i+4]
+            buyerSequences[ sequence ] = prices[sn+4]
 
+    # get the total sold for all matching sequences for this buyer
     for k, v in buyerSequences.items():
         if k not in bananas:
             bananas[k] = v
         else:
             bananas[k] += v
 
-print(f"Max bananas: {max(bananas.values())}")
+print(f"PART2: Max bananas: {max(bananas.values())}")
