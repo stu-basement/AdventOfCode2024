aa1ca8bbc958b201799f15e2964c2ced8ef32cac
Stu Fleming <stu@thebasement.nz>
2025-01-05 16:21:22 +1300

    Refactored

--- day25/day25.py
+++ day25/day25.py
@@ -1,13 +1,14 @@
+""" AdventOfCode Day 25 """
 keys = []
 locks = []
 
-with open("input", "r") as f:
+with open("input", "r", encoding="utf-8") as f:
     line = f.readline()
     while len(line) > 1:
         if line[0] == '#':
             # read a lock
             lock = [0,0,0,0,0]
-            for l in range(5):
+            for _ in range(5):
                 line = f.readline()
                 for p in range(5):
                     if line[p] == '#':
@@ -16,7 +17,7 @@ with open("input", "r") as f:
         elif line[0] == '.':
             # read a key
             key = [0,0,0,0,0]
-            for l in range(5):
+            for _ in range(5):
                 line = f.readline()
                 for p in range(5):
                     if line[p] == '#':
@@ -28,24 +29,5 @@ with open("input", "r") as f:
         line = f.readline()
         line = f.readline()
 
-def keyFits(k, l):
-    for p in range(5):
-        if k[p] > 5 - l[p]:
-            return False
-    return True
-
-def keyOverlaps(k, l):
-    for p in range(5):
-        if k[p] + l[p] > 5:
-            return True
-    return False
-
-print(f"Locks: {locks}")
-print(f"Keys: {keys}")
-
-lockKeyPair =  0
-for l in locks:
-    for k in keys:
-        if keyFits(k, l) and not keyOverlaps(k, l):
-            lockKeyPair += 1
-print(f"Total pairs: {lockKeyPair}")
+print("PART1: ", sum( all(key[p] <= 5 - lock[p] for p in range(5)) and not \
+        all(key[p] + lock[p] > 5 for p in range(5)) for lock in locks for key in keys ))
