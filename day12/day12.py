a85cbfd011102ff0ffaed41699ec4ef61d7c829c
Stu Fleming <stu@thebasement.nz>
2025-01-10 18:03:20 +1300

    Refactored and simplified using list comprehension, convolution for part 2 to detect corners

--- day12/day12.py
+++ day12/day12.py
@@ -1,210 +1,88 @@
+""" AdventOfCode Day 12 """
+from scipy.signal import convolve2d
 import numpy as np
 
 def onMap(m, x, y):
+    """ Check that a square is on the map """
     return 0 <= x < len(m[0]) and 0 <= y < len(m)
 
-directions = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]
 orthogonalDirections = [[0,-1],[1,0],[0,1],[-1,0]]
+regionMap = []
 
-def validMove(m, x, y, dx, dy, r):
-    if onMap(m, x+dx, y+dy):
-        cell = m[y+dy][x+dx]
-        if (cell == r):
-            return True
-    return False
+def validMove(m, rx, ry, dx, dy, r_id):
+    """ Check that the square in dx, dy direction is still part of this region """
+    return onMap(m, rx+dx, ry+dy) and m[ry+dy][rx+dx] == r_id
 
-def adjacentInRegion(region, x, y, dx, dy, r):
-    if (x+dx, y+dy, r) in region:
-        return True
-    else:
-        return False
-
-def regionBoundingBox(region):
-    bottomLeftX = min(point[0] for point in region)
-    bottomLeftY = min(point[1] for point in region)
-    topRightX = max(point[0] for point in region)
-    topRightY = max(point[1] for point in region)
+def regionBoundingBox(r):
+    """ Calculate the rectangular bounding box for a region """
+    bottomLeftX = min(point[0] for point in r)
+    bottomLeftY = min(point[1] for point in r)
+    topRightX = max(point[0] for point in r)
+    topRightY = max(point[1] for point in r)
 
     return (bottomLeftX, bottomLeftY), (topRightX, topRightY)
 
-def printRegion(region):
-    regionMap = []
-
-    bottomLeft, topRight = regionBoundingBox(region)
-    for i in range(abs(topRight[1] - bottomLeft[1]) + 1):
-        regionMap.append(list(''.ljust(abs(topRight[0] - bottomLeft[0]) + 1, '.')))
-
-    pointIter = iter(region)
-    r = next(pointIter, None)
-    while (r != None):
-        regionMap[r[1] - bottomLeft[1]][r[0] - bottomLeft[0]] = r[2]
-        r = next(pointIter, None)
-
-    return regionMap
-
-def scanRegion(region, m, x, y, r):
-    if m[y][x] in '0.':
-        return None
-
+def scanRegion(r, m, startX, startY, r_id):
+    """ Scan all of the connected points for a region """
     stack = []
-    stack.append((x,y,r))
-    while len(stack) > 0:
+    stack.append( (startX,startY,r_id) )
+    while stack:
         point = stack.pop()
-        if not point in(region) and (point[2] == r):
-            region.add(point)
+        if point not in r and (point[2] == r_id):
+            r.add(point)
 
-            for d in orthogonalDirections:
-                if validMove(m,point[0],point[1],d[0],d[1],r):
-                    neighbor = ((point[0]+d[0], point[1]+d[1], r))
+            for dx, dy in orthogonalDirections:
+                if validMove(m,point[0],point[1],dx,dy,r_id):
+                    neighbor = ((point[0]+dx, point[1]+dy, r_id))
                     stack.append(neighbor)
 
     return region
-    
-def calcPerimeter(m, region):
-    perimeter = 0
-    for point in region:
-        adjacentCount = 0
-        for d in orthogonalDirections:
-            if adjacentInRegion(region, point[0], point[1], d[0], d[1], point[2]):
-                adjacentCount += 1 
 
-        if (adjacentCount < 0) or (adjacentCount > 4):
-            print(f"ERROR at {point[0]},{point[1]} {adjacentCount}")
-            for row in regionMap:
-                print(f"{''.join(str(c) for c in row)}")
-
-            exit()
-
-        # handle the case where part of a region isn't orthogonally connected
-        if (adjacentCount == 0) and (len(region) > 1):
-            adjacentCount = 4
-
-        regionMap[point[1]][point[0]] = str(adjacentCount)
-
-        perimeter += (4 - adjacentCount)
-    return perimeter
-
-areaMap = []
-with open("input", "r") as f:
+with open("input", "r", encoding="utf-8") as f:
     for line in f.readlines():
-        areaMap.append(list(line.replace('\n','')))
+        regionMap.append(list(line.replace('\n','')))
 
-regionMap = areaMap.copy()
+map_width = len(regionMap[0])
+map_height = len(regionMap)
 
-print(f"Original map {len(regionMap)} rows of {len(regionMap[0])} columns")
-for row in regionMap:
-    print(f"{''.join(str(c) for c in row)}")
-
-print("-----")
-
+# isolate each region and remove from the map as we go
 regions = []
-for y in range(0,len(areaMap)):
-    for x in range(0, len(areaMap[0])):
-        r = areaMap[y][x]
-
-        region = set()
-        region = scanRegion(region, regionMap, x, y, r)
-        if region != None:
+for y in range(map_height):
+    for x in range(map_width):
+        # each region is a set of (x, y, r) where x, y is each point
+        # contained in the region identified by letter in r
+        if regionMap[y][x] not in '0.':
+            region = set()
+            region = scanRegion(region, regionMap, x, y, regionMap[y][x])
             regions.append(region)
             for p in region:
                 regionMap[p[1]][p[0]] = '.'
 
-totalCost = 0
-for r in regions:
-    area = len(r)
-    perimeter = calcPerimeter(regionMap, r)
-    totalCost += (area * perimeter)
-
-for row in regionMap:
-    print(f"{''.join(str(c) for c in row)}")
-
+totalCost = sum( len(r) * sum( 4 - sum( (px + dx, py + dy, pr) in r \
+            for dx, dy in orthogonalDirections ) \
+            for px, py, pr in r ) for r in regions )
 print(f"{len(regions)} regions found")
 
-print(f"Total cost {totalCost}")
+print(f"PART1: Total cost {totalCost}")
 
 print("PART2")
-# Key insmatrix multiplication 2x2ight for part 2 is that the number of sides is the same as the number of corners
-#
-# There are eight corner cases
-#
-# 1 1 and 1 1
-# 1 X     X 1 are corners (1 is inside region, X is outside of region)
-#
-# 1 X     X 1
-# 1 1     1 1
-#
-# X X     X X
-# X 1     1 X
-#
-# X 1     1 X
-# X X     X X
-insideCorners = { ( 0, 0, 0, 0, 1, 1, 0, 1, 0 ), ( 0, 1, 0, 0, 1, 1, 0, 0, 0 ), ( 0, 1, 0, 1, 1, 0, 0, 0, 0 ), ( 0, 1, 0, 1, 1, 0, 0, 1, 0 ) }
-uturns = { ( 0, 0, 0, 1, 1, 0, 0, 0, 0 ), ( 0, 0, 0, 0, 1, 1, 0, 0, 0), ( 0, 1, 0, 0, 1, 0, 0, 0, 0 ), ( 0, 0, 0, 0, 1, 0, 0, 1, 0 ) }
-outsideCorners = { ( 0, 0, 0, 0, 1, 1, 0, 1, 0 ), ( 0, 0, 0, 1, 1, 0, 0, 1, 0), (0, 1, 0, 1, 1, 0, 0, 0, 0), ( 0, 1, 0, 0, 1, 1, 0, 0, 0 ) } 
-tees = { ( 0, 1, 0, 0, 1, 1, 0, 1, 0 ), ( 0, 0, 0, 1, 1, 1, 0, 1, 0), ( 0, 1, 0, 1, 1, 0, 0, 1, 0 ), ( 0, 1, 0, 1, 1, 1, 0, 0, 0 ) }
-singleton = { ( 0, 0, 0, 0, 1, 0, 0, 0, 0) }
-
-regionMap = []
-with open("input", "r") as f:
-    for line in f.readlines():
-        regionMap.append(list(line.replace('\n','')))
-
-for row in regionMap:
-    print(f"{''.join(str(c) for c in row)}")
-
-regions = []
-for y in range(0,len(regionMap)):
-    for x in range(0, len(regionMap[0])):
-        r = regionMap[y][x]
-
-        region = set()
-        region = scanRegion(region, regionMap, x, y, r)
-        if region != None:
-            regions.append(region)
-            for p in region:
-                regionMap[p[1]][p[0]] = '0'
+# Kernel for detecting corners i [-1, 1], [1, -1]
+# For each region, make a 0/1 map of the points it contains
+# Convolve the kernel over the pa to count the corners per region
 
 totalPrice = 0
+corner_kernel = [ [-1,1], [1, -1] ]
 for region in regions:
-    calcPerimeter(regionMap, region)
-    totalCorners = 0
-    pointIter = iter(region)
-    p = next(pointIter, None)
+    # start with a blank map sized to the region
+    (blX, blY), (trX, trY) = regionBoundingBox(region)
+    region_points = np.zeros( (abs(trX - blX) + 1, abs(trY - blY) + 1) )
 
-    prevCorners = totalCorners
-    while p != None:
-        # corner detection
-        # put feature at centre of 3x3 matrix
-#        feature = ( 1 if (p[0]-1, p[1]-1, p[2]) in region else 0, 1 if (p[0],p[1]-1,p[2]) in region else 0, 1 if (p[0]+1,p[1]-1,p[2]) in region else 0,
-#                    1 if (p[0]-1, p[1],   p[2]) in region else 0, 1 if (p[0],p[1],  p[2]) in region else 0, 1 if (p[0]+1,p[1],  p[2]) in region else 0,
-#                    1 if (p[0]-1, p[1]+1, p[2]) in region else 0, 1 if (p[0],p[1]+1,p[2]) in region else 0, 1 if (p[0]+1,p[1]+1,p[2]) in region else 0 )
-        feature = ( 0, 1 if (p[0],p[1]-1,p[2]) in region else 0, 0,
-                    1 if (p[0]-1, p[1],   p[2]) in region else 0, 1 if (p[0],p[1],  p[2]) in region else 0, 1 if (p[0]+1,p[1],  p[2]) in region else 0,
-                    0, 1 if (p[0],p[1]+1,p[2]) in region else 0, 0 )
+    # fill 1s for every point in the region
+    for x,y,_ in region:
+        region_points[abs(x - blX)][abs(y - blY)] = 1
 
-        if feature in insideCorners:
-            totalCorners += 1
-        if feature in uturns:
-            totalCorners += 2
-        if feature in outsideCorners:
-            totalCorners += 1
-        if feature in tees:
-            totalCorners += 2
-        if feature in singleton:
-            totalCorners += 4
- 
-        p = next(pointIter, None)
+    # convolve with the corner-detection kernel to yield +/-1 at each corner
+    result = convolve2d(region_points, corner_kernel)
+    totalPrice += int(len(region) * np.sum(np.abs(result)))
 
-    if (prevCorners == totalCorners):
-        print(f"ERROR: No corners added for this region {region}")
-
-    totalPrice += (len(region) * totalCorners)
-      
-print(f"Total discounted price {totalPrice}")
-
-for r in regions:
-    bottomLeft, topRight = regionBoundingBox(r)
-    print(f"Region bounding box {bottomLeft[0]},{bottomLeft[1]} to {topRight[0]},{topRight[1]}")
-    regionMap = printRegion(r)
-    for r in regionMap:
-        print(''.join(r))
+print(f"PART2: Total discounted price {totalPrice}")
